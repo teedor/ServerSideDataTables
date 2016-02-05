d880a4177bb1e1258d05683e1bb7825dc62de60a
Stephen Anderson <s.anderson@kainos.com>
2016-02-05 14:54:54 +0000

    added web project

---
+++ Skippy/SkippyWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SkippyWeb.Startup))]
+namespace SkippyWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
