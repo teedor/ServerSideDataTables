5d95212dc2b0fee224d888d5744be2b90724132c
Steven Alexander <s.alexander@kainos.com>
2016-02-11 11:28:17 +0000

    added custom person searching

--- SkippyWeb/Controllers/HomeController.cs
+++ SkippyWeb/Controllers/HomeController.cs
@@ -66,7 +66,7 @@ namespace ServerSideDatatables.Controllers
                 {
                     draw = draw,
                     recordsTotal = recordsTotal,
-                    recordsFiltered = recordsTotal,
+                    recordsFiltered = recordsFiltered,
                     data = data
                 };
 
--- SkippyWeb/Datatables/Repository/PeopleDatatablesRepository.cs
+++ SkippyWeb/Datatables/Repository/PeopleDatatablesRepository.cs
@@ -1,4 +1,7 @@
 ﻿
+using System.Data.Entity.SqlServer;
+using System.Linq;
+
 namespace ServerSideDatatables.Datatables.Repository
 {
     public class PeopleDatatablesRepository : DatatablesRepository<Person>
@@ -6,6 +9,17 @@ namespace ServerSideDatatables.Datatables.Repository
         public PeopleDatatablesRepository(SkippyEntities context)
             : base(context)
         {
+        }
+
+        protected override IQueryable<Person> GetWhereQueryForSearchValue(IQueryable<Person> queryable, string searchValue)
+        {
+            return queryable.Where(x =>
+                    // id column (int)
+                    SqlFunctions.StringConvert((double)x.Id).Contains(searchValue)
+                    // name column (string)
+                    || x.Name.Contains(searchValue)
+                    // date of birth column (datetime, formatted as d/M/yyyy) - limitation of sql prevented us from getting leading zeros in day or month
+                    || (SqlFunctions.StringConvert((double)SqlFunctions.DatePart("dd", x.DateOfBirth)) + "/" + SqlFunctions.DatePart("mm", x.DateOfBirth) + "/" + SqlFunctions.DatePart("yyyy", x.DateOfBirth)).Contains(searchValue));
         }
     }
 }
